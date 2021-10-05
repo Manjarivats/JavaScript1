65246975f55cfeb4f672f3f5b67bfc18b74a0541
Manjari28 <manjari.vatsyayan28@gmail.com>
2021-10-05 11:18:06 +0530

    function.js added

---
+++ function.js
@@ -0,0 +1,24 @@
+function doSomething(){
+    console.log("do somethins");
+}
+doSomething();
+doSomething();
+
+//function without argument and no returb value
+
+function doSomething(whatToDo){
+    console.log("Doing :"+whatToDo);
+
+}
+doSomething("Drink coffee");doSomething("Write Code");
+
+function add(){
+    var n1 =20;
+    var n2 = 10;
+    var sum = n1+n2;
+    console.log(sum);
+}
+add();
+add();
+
+
