68b0acfabffe9d14688eaf165fda247ad74b5a64
Manjari28 <manjari.vatsyayan28@gmail.com>
2021-10-05 10:58:04 +0530

    op3.js added

---
+++ op3.js
@@ -0,0 +1,12 @@
+var age =18;
+if (age<18 || age>30)
+console.log("not eligible ");
+else
+console.log(" eligible ");
+
+if (age>=18 && age<=30)
+console.log(" eligible ");
+else
+console.log("not eligible ");
+console.log(!true);
+console.log(!false);
